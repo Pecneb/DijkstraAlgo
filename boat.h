61e03507c529b8f6dbf02163de3d17fbfd5840f5
Parrot User <user@localhost.localdomain>
2021-08-05 13:09:48 +0100

    main human boat created

---
+++ boat.h
@@ -0,0 +1,15 @@
+#ifndef BOAT
+#define BOAT
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+struct boat {
+  size_t size;
+  size_t ppl_num;
+  size_t can_num;
+  size_t veg_num;
+};
+
+#endif
---
+++ human.h
@@ -0,0 +1,12 @@
+#ifndef HUMAN
+#define HUMAN
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct human {
+  bool vegan;
+  bool cannibal;
+}; 
+
+#endif
